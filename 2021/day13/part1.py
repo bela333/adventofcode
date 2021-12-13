0f7fc5a95586ed8c3bc57ed54950b5e21951f65a
bela333 <b3kstudio@gmail.com>
2021-12-13 08:27:13 +0100

    day13

---
+++ 2021/day13/part1.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+with open("input.txt") as f:
+    lines = [line.strip() for line in f.readlines()]
+    coords = []
+    #parse coordinates
+    for l in lines:
+        if l == '':
+            break
+        [x, y] = l.split(",")
+        coords.append((int(x), int(y)))
+    width = max([x for x, y in coords])+1
+    height = max([y for x, y in coords])+1
+    field = np.full((height, width), False, bool)
+    for x, y in coords:
+        field[y, x] = True
+    #apply mirrors
+    for i in range(lines.index('')+1, len(lines)):
+        l = lines[i]
+        num = int(l[13:])
+        if l[11] == 'y':
+            a = np.flip(field[num+1:,:], 0)
+            field = field[:num,:]
+            field = field | a
+        else:
+            a = np.flip(field[:,num+1:], 1)
+            field = field[:,:num]
+            field = field | a
+        break
+    print(field.sum())
