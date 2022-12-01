761f371d03abb8e3ee5ea28689c040a1b10140be
bela333 <b3kstudio@gmail.com>
2022-12-01 10:37:31 +0100

    2022 Day 1

---
+++ 2022/day1/part2.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn c [] = [[]]
+splitOn c (x:xs)
+    | x == c    = []: splitOn c xs
+    | otherwise = (x:h):t where (h:t) = splitOn c xs
+
+splitLines :: String -> [String]
+splitLines = splitOn '\n'
+
+main :: IO ()
+main = do 
+    file <- readFile "input.txt"
+    let lines = splitLines file
+    let elves = splitOn "" lines
+    let elves' = (map.map) read elves :: [[Integer]]
+    let sums = map sum elves'
+    print $ sum $ take 3 $ reverse $ sort sums
