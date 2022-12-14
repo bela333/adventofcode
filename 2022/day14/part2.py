8a2c918f7da82e91bb704f4c85ded9d60bfba71f
bela333 <b3kstudio@gmail.com>
2022-12-14 10:57:06 +0100

    2022 Day 14

---
+++ 2022/day14/part2.py
@@ -0,0 +1,76 @@
+class Field:
+    def __init__(self) -> None:
+        self.field = {}
+        self.min = None
+        self.max = None
+    
+    def update_bounds(self, coords):
+        if self.min is None or self.max is None:
+            self.min = coords
+            self.max = coords
+            return
+        self.min = (min(self.min[0], coords[0]), min(self.min[1], coords[1]))
+        self.max = (max(self.max[0], coords[0]), max(self.max[1], coords[1]))
+    
+    def put_wall(self, coords):
+        self.field[coords] = "#"
+        self.update_bounds(coords)
+    
+    def simulate_sand(self, coords):
+        (x, y) = coords
+        alive = True
+        
+        while alive:
+            if not self.is_blocked((x, y+1)): # Not blocked below?
+                y += 1
+            elif not self.is_blocked((x-1, y+1)): # Not blocked to the left?
+                y += 1
+                x -= 1
+            elif not self.is_blocked((x+1, y+1)): # Not blocked to the right?
+                y += 1
+                x += 1
+            else:
+                alive = False # Could not move anywhere
+        self.put_sand((x, y))
+        return (x, y) == (500, 0)
+
+    
+    def put_sand(self, coords):
+        self.field[coords] = "+"
+    
+    def is_blocked(self, coords) -> bool:
+        if coords[1] == self.max[1]+2:
+            return True
+        return coords in self.field
+    
+    def draw_rectangle(self, fromcoords, tocoords):
+        (fromx, fromy) = fromcoords
+        (tox, toy) = tocoords
+        fromx,tox = min(fromx, tox), max(fromx, tox)
+        fromy,toy = min(fromy, toy), max(fromy, toy)
+        for x in range(fromx, tox+1):
+            for y in range(fromy, toy+1):
+                self.put_wall((x, y))
+    def draw_line(self, line):
+        for i in range(len(line)-1):
+            fromcoords = line[i]
+            tocoords = line[i+1]
+            self.draw_rectangle(fromcoords, tocoords)
+
+            
+
+def parse_line(line):
+    return [tuple(map(int, a.split(","))) for a in line.split(" -> ")]
+
+with open("input.txt") as f:
+    data = [parse_line(a) for a in f.readlines()]
+
+field = Field()
+
+for line in data:
+    field.draw_line(line)
+
+i = 0
+while( not field.simulate_sand((500, 0))):
+    i += 1
+print(len([a for a in field.field.values() if a == "+"]))
